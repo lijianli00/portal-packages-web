6b4d489d81b188029de898ef3a30b718c2618988
李建立 <lijianli0427@163.com>
2023-11-28 16:35:58 +0800

    修改layout

--- examples/layout/index.vue
+++ examples/layout/index.vue
@@ -1,7 +1,20 @@
+<!-- 自定义布局 需要重新组装 
+<template>
+  <ShLayout :logoUrl>
+    写自定义布局内容
+  </ShLayout>
+</template>
+-->
+<!-- 本框架默认布局
 <template>
   <ShLayout></ShLayout>
 </template>
+-->
+<template>
+  <ShLayout> </ShLayout>
+</template>
 <script>
+import logoUrl from '../assets/3hlogo.png'
 import { ShLayout } from 'blackbox-ui'
 export default {
   components: {
@@ -10,7 +23,34 @@ export default {
   data() {
     return {}
   },
-  methods: {}
+  mounted() {
+    const setting = {
+      logoUrl: logoUrl, //标题图标
+      breadHeader: 'breadcrumb' //breadHeader等于breadcrumb 为面包屑导航 tags为标签导航
+    }
+    this.$store.commit('configStore/SET_Setting', setting)
+  },
+  methods: {
+    // 例子   登录之后 跳转 指定路由界面   或者使用 调用 登录之后的方法
+    // <ShLayout :is-customize="true" @callback="callback"></ShLayout>
+    // callback(value) {
+    //   this.$router.push('/bbx/platform/appClient')
+    //   let data = {
+    //     checkedSvg: null,
+    //     children: null,
+    //     grade: 2,
+    //     id: '8e44ffb862ec4bdab6e99e6287dbeacf',
+    //     menuName: '应用管理',
+    //     parentId: '923daa6c6c6540bfbfed9ee6441b1226',
+    //     remark: '',
+    //     sortNum: -1,
+    //     svg: '',
+    //     uncheckedSvg: null,
+    //     url: '/bbx/platform/appClient'
+    //   }
+    //   this.$store.commit('tagsStore/addTag', data)
+    // }
+  }
 }
 </script>
 
--- examples/router/module/index.js
+++ examples/router/module/index.js
@@ -1,7 +1,25 @@
-import { RouterBlackbox } from 'blackbox-ui'
+import { RouterCustomLayout, RouterBaseCustom } from 'blackbox-ui'
 import Layout from '../../layout/index.vue'
 
 export default [
+  {
+    path: '/',
+    meta: { title: '布局', notLogin: true },
+    component: Layout,
+    children: [
+      {
+        path: '/bbx/portal',
+        meta: { title: '列表' },
+        component: () => import('../../views/portal/list/index.vue')
+      },
+      {
+        path: '/bbx/index/:id',
+        meta: { title: '首页' },
+        component: () => import('../../views/portal/index/index.vue')
+      },
+      ...RouterCustomLayout
+    ]
+  },
   {
     path: '/login',
     meta: { title: '登录', notLogin: true },
@@ -22,22 +40,5 @@ export default [
     meta: { title: '新增', notAuth: true },
     component: () => import('../../views/portal/add/index.vue')
   },
-  {
-    path: '/layout',
-    meta: { title: '布局', notLogin: true },
-    component: Layout,
-    children: [
-      {
-        path: '/bbx/portal',
-        meta: { title: '列表' },
-        component: () => import('../../views/portal/list/index.vue')
-      },
-      {
-        path: '/bbx/index/:id',
-        meta: { title: '首页' },
-        component: () => import('../../views/portal/index/index.vue')
-      }
-    ]
-  },
-  ...RouterBlackbox // 一定要放在最后面
+  ...RouterBaseCustom
 ]
