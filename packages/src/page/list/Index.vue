0cfa2c726a784976c2fb83770fa6e730415b4624
李建立 <lijianli0427@163.com>
2023-12-07 16:41:23 +0800

    23

--- packages/src/page/list/Index.vue
+++ packages/src/page/list/Index.vue
@@ -26,7 +26,7 @@
       <sh-table-column label="操作" width="180">
         <template slot-scope="scope">
           <div class="table-btn">
-            <el-button type="text" @click="onUpdate(scope.row)">编辑</el-button>
+            <el-button type="text" @click="onUpdate(scope.row)">修改</el-button>
             <el-button class="del-btn" type="text" @click="onDelete(scope.row)">删除</el-button>
           </div>
         </template>
